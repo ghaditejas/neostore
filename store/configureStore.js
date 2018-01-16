a39b1278ecf2cf3a64aaabe262486e23a1662d4b
Tejas Ghadigaonkar <tejas.ghadigaonkar@neosofttech.com>
2018-01-16 20:00:13 +0530

    install thunk

--- store/configureStore.js
+++ store/configureStore.js
@@ -1,9 +1,11 @@
 import {createStore} from 'redux';
 import rootReducer from '../reducers'
-// import reduxImmmutableStateInvariant from 'redux-immutable-state-invariant';
+import reduxImmmutableStateInvariant from 'redux-immutable-state-invariant';
+import 	thunk from 'redux-thunk';
+
 export default function configureStore(initialState) {
 	return createStore(rootReducer,
 		initialState,
-		 //applyMiddleware(reduxImmmutableStateInvariant())
+		 applyMiddleware(thunk,reduxImmmutableStateInvariant())
 		);
 }
